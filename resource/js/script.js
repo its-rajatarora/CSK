23417b3c6db983e9ce189c9d1fd030dfb4f085da
its-rajatarora <rajatjune99@gmail.com>
2023-08-21 23:26:54 +0530

    repo init

---
+++ resource/js/script.js
@@ -0,0 +1,77 @@
+ $('.js--dhoni').click(function(){
+        $('html,body').animate({scrollTop: $('.js--c-dhoni').offset().top},1000);
+    })
+    $('.js--jadeja').click(function(){
+        $('html,body').animate({scrollTop: $('.js--vc-jadeja').offset().top},1000);
+    })
+
+ $('.js--team').click(function(){
+        $('html,body').animate({scrollTop: $('.js--p-team').offset().top},1000);
+    })
+$('.js--login').click(function(){
+        $('html,body').animate({scrollTop: $('.js--form').offset().top},1000);
+    })
+    $('.js--ipl').click(function(){
+        $('html,body').animate({scrollTop: $('.js--c-ipl').offset().top},1000);
+    })
+    
+
+    let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+//  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+//  for (i = 0; i < dots.length; i++) {
+//    dots[i].className = dots[i].className.replace(" active", "");
+//  }
+  slides[slideIndex-1].style.display = "block";
+//  dots[slideIndex-1].className += " active";
+}
+
+//function myFunction() {
+//  var x = document.getElementById("myDIV");
+//  if (x.style.display === "none") {
+//    x.style.display = "block";
+//  } else {
+//    x.style.display = "none";
+//  }
+//}
+
+
+// let slideIndex = [1,1];
+// /* Class the members of each slideshow group with different CSS classes */
+// let slideId = ["mySlides1", "mySlides2"]
+// showSlides(1, 0);
+// showSlides(1, 1);
+
+// function plusSlides(n, no) {
+//   showSlides(slideIndex[no] += n, no);
+// }
+
+// function showSlides(n, no) {
+//   let i;
+//   let x = document.getElementsByClassName(slideId[no]);
+//   if (n > x.length) {slideIndex[no] = 1}
+//   if (n < 1) {slideIndex[no] = x.length}
+//   for (i = 0; i < x.length; i++) {
+//     x[i].style.display = "none";
+//   }
+//   x[slideIndex[no]-1].style.display = "block";
+// }
